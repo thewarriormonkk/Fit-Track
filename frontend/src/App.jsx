531b7c41209a1c2c2a8afba5f8092c5029773295
Satendra Mishra <msatendra424@gmail.com>
2026-01-10 18:13:48 +0530

    feat(client): setup React frontend with Vite and React Router v6
    - initialize React app using Vite
    - configure basic client-side routing
    - add initial pages and components structure for separation of concerns

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+// pages
+import Home from './pages/Home';
+
+function App() {
+
+  return (
+    <div className='App' >
+      <Navbar />
+      <div className="pages">
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  )
+}
+
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div>
+            <div className="home">
+                <h2>Home</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
