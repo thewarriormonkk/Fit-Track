cd4b03d4c2f533c81fa960b93f821210526db012
thewarriormonkk <83344077+thewarriormonkk@users.noreply.github.com>
2026-01-12 10:12:48 +0530

    feat(frontend): fetch and display workouts from backend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from 'react';
+import WorkoutDetails from '../components/WorkoutDetails';
+
+
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null);
+    const API_URL = import.meta.env.VITE_API_URL;
+
+    useEffect(() => {
+        async function fetchWorkouts() {
+            const response = await fetch(API_URL);
+            const data = await response.json();
+            if (response.ok) {
+                setWorkouts(data);
+            }
+        }
+        fetchWorkouts();
+    }, []);
+
     return (
-        <div>
             <div className="home">
-                <h2>Home</h2>
+                <div className="workouts">
+                    {workouts && workouts.map((workout) => (
+                        <WorkoutDetails key={workout._id} workout={workout} />
+                    ))}
+                </div>
             </div>
-        </div>
     );
 }
 
