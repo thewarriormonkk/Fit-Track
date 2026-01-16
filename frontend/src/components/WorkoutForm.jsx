38cf4eac014efc784350222b589a3e2e94626783
thewarriormonkk <83344077+thewarriormonkk@users.noreply.github.com>
2026-01-16 15:14:40 +0530

    feat(ui): highlight invalid form fields from backend validation

--- frontend/src/components/WorkoutForm.jsx
+++ frontend/src/components/WorkoutForm.jsx
@@ -8,6 +8,7 @@ function WorkoutForm() {
     const [load, setLoad] = useState('');
     const [reps, setReps] = useState('');
     const [error, setError] = useState(null);
+    const [emptyFields, setEmptyFields] = useState([]);
 
     const API_URL = import.meta.env.VITE_API_URL;
 
@@ -24,9 +25,11 @@ function WorkoutForm() {
 
         if (!response.ok) {
             setError(data.message);
+            setEmptyFields(data.emptyFields);
         }
 
         if (response.ok) {
+            setEmptyFields([]);
             setError(null);
             setTitle('');
             setReps('');
@@ -46,18 +49,21 @@ function WorkoutForm() {
                     type="text" 
                     onChange={(e) => setTitle(e.target.value)} 
                     value={title} 
+                    className={emptyFields.includes('title') ? 'error' : '' }
                 />
                 <label>Number of Reps: </label>
                 <input 
                     type="number" 
                     onChange={(e) => setReps(e.target.value)} 
                     value={reps}
+                    className={emptyFields.includes('reps') ? 'error' : '' }
                 />
                 <label>Load (in kg): </label>
                 <input 
                     type="number" 
                     onChange={(e) => setLoad(e.target.value)}
                     value={load}
+                    className={emptyFields.includes('load') ? 'error' : '' }
                 />
                 <button>Add Workout</button>
                 {error && <div className='error'>{error}</div>}
