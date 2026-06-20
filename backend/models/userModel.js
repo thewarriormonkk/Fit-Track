b75086462cd3e28f933e1a1b219eba908c39a3b9
Satendra <thewarriormonkk@users.noreply.github.com>
2026-06-20 19:37:11 +0530

    feat(auth): implement user signup with password hashing

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const { Schema } = mongoose;
 
@@ -13,6 +14,21 @@ const userSchema = new Schema({
         required: true
     }
 });
+
+userSchema.statics.signup = async function (email, password) {
+
+    const exists = await this.findOne({ email });
+
+    if (exists) {
+        throw new Error("Email already in use!");
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    const user = await this.create({ email, password: hash });
+
+    return user;
+}
 
 module.exports = mongoose.model('User', userSchema);
 
